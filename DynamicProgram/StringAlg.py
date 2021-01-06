23a7c000cd48b197eb0a18307352d0067eba756e
wuzhuang <15091165668@163.com>
2021-01-06 17:28:11 +0800

    test

--- DynamicProgram/StringAlg.py
+++ DynamicProgram/StringAlg.py
@@ -12,6 +12,7 @@
 # num1 和num2 都只包含数字  0-9
 # num1 和num2 都不包含任何前导零
 # 你不能使用任何內建 BigInteger 库，也不能直接将输入的字符串转换为整数形式
+import networkx as nx
 
 def Reverse(s:str) -> str:
     """
@@ -79,13 +80,171 @@ def addstr( num1: str, num2: str):
     return "".join(ans[::-1])
 
 """
-字符串相乘
+字符串相乘，使用字符串相加算法
 """
+# def multiply( num1: str, num2: str) -> str:
 
+"""
+较大分组的位置
+"""
+# def largeGroupPositions( s: str):
+#     ret = list()
+#     n, num = len(s), 1
+#
+#     for i in range(n):
+#         if i == n - 1 or s[i] != s[i + 1]:
+#             if num >= 3:
+#                 ret.append([i - num + 1, i])
+#             num = 1
+#         else:
+#             num += 1
+#
+#     return ret
+#
 
+#较大分组
+def largeGroupPositions(s: str):
+    nl = 1
+    ans = []
+    for i in range(len(s)):
+        if i==len(s)-1 or s[i]!=s[i+1]:
+            if nl>=3:
+                ans.append([i-nl+1,i])
+            nl=1
+        else:
+            nl+=1
+    return ans
+#Z字形变换
+def convert( s: str, numRows: int):
+    if numRows == 1:
+        return s
+    sl = len(s)
+    ans = []
+    index = 1
+    dir = True
+    xxx={}
+    for i in range(numRows):
+        xxx[i+1] = []
+    for i in range(sl):
+        if dir == True:
+            ans.append(index)
+            index+=1
+            if index==numRows:
+                dir = False
+        if dir == False :
+            ans.append(index)
+            index -=1
+            if index == 1:
+                dir = True
+    for i in range(sl):
+        xxx[ans[i]].append(s[i])
+    xxxx = ""
+    for i in range(numRows):
+        xxxx = xxxx + ''.join(xxx[i+1])
+    print(xxxx)
+    print(ans)
 
+#是有效数独
 
+# def isValidSudoku(self, board):
+#     row = [{}for i in range(9)]
+#     col = [{}for i in range(9)]
+#     box = [{}for i in range(9)]
+#     for i in range(9):
+#         for j in range(9):
+#             if board[i][j] != '.':
+#                 num = int(board[i][j])
+#                 box_index = (i//3)*3 + j//3
+#                 rows[i][num] = rows[i].get(num, 0) + 1
+#                 columns[j][num] = columns[j].get(num, 0) + 1
+#                 boxes[box_index][num] = boxes[box_index].get(num, 0) + 1
+#
+#                 if rows[i][num] > 1 or columns[j][num] > 1 or boxes[box_index][num] > 1:
+#                     return False
+#     return True
 
+#数字条约
+def canJump(nums) -> bool:
+    max_pos = nums[0]+0
+    for i in range(1,len(nums)):
+        if i <= max_pos:
+            if i+nums[i] >= max_pos:
+                max_pos = i+nums[i]
 
+    return max_pos>=len(nums)-1
 
-print(addstr("123","1239"))
+#能否环路
+#起始点的汽油两要大于起始点的消耗
+def canCompleteCircuit(gas, cost):
+    for i in range(len(gas)):
+        if gas[i]>=cost[i]:
+            if isCircuit(i,gas,cost):
+                return i
+    return -1
+def isCircuit(i,gas,cost):
+    endpos = -1
+    currentpos = i
+    currentoil = gas[i]
+    totalnums = len(gas)
+    for j in range(totalnums):
+        if currentoil >=cost[currentpos]:
+            currentpos = currentpos+1 if(currentpos+1 <totalnums) else 0
+            lastpos = currentpos-1 if(currentpos-1>=0) else totalnums-1
+            currentoil = currentoil - cost[lastpos] + gas[currentpos]
+            endpos = currentpos
+
+    return endpos
+#链表问题
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        return ListNode(0)
+#有效字符串
+
+#最大水容量
+
+def maxArea(height) -> int:
+    nums = len(height)
+    start = 0
+    end = nums-1
+    maxa = -9999
+    while start <= end:
+        if maxa < Aera(start,height[start],end,height[end]):
+            maxa = Aera(start,height[start],end,height[end])
+        if height[start] > height[end]:
+            end -= 1
+        else:
+            start+=1
+    return maxa
+
+def Aera(x1,y1,x2,y2):
+    return (x2-x1)*min(y1,y2)
+#外观数列
+def countAndSay(n: int) -> str:
+    if n==1:
+        return "1"
+    else:
+        tempstr = countAndSay(n-1)
+        return cutSlpit(tempstr)
+def cutSlpit(s):
+    nums = len(s)
+    ans=[]
+    temp = ""
+    for i in range(nums):
+        temp+=s[i]
+        if i<= nums-2 and s[i] == s[i+1]:
+            continue
+        ans.append(temp)
+        temp = ""
+    anstr = ""
+    for i in ans:
+        anstr = anstr+str(len(i))+str(i[0])
+    return anstr
+
+if __name__ == "__main__":
+    print(countAndSay(5))
